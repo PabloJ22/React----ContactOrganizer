e1ad3dfd3d3d4399a4412df2ae678b7aff74391f
APabloJ <altisimop@gmail.com>
2019-07-14 15:58:33 -0500

    connected MongoDB to application. Sucess.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => 
 res.json({msg: 'Welcome to your most efficient Contact Organizer the Internet can Supply.'})
