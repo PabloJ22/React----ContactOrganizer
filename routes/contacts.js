6a970b5da13b9f2ee1dd0ba6a894fb3e16d1f52d
APabloJ <altisimop@gmail.com>
2019-07-15 11:25:02 -0500

    api completed. jwt is set up, now we can fetch token and access protected routes with that token. Back-End is not complete. Success.

--- routes/contacts.js
+++ routes/contacts.js
@@ -60,16 +60,63 @@ try{
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
-router.put ('/', (req, res) => {
-  res.send('Update contacts');
+router.put ('/:id', auth, async(req, res) => {
+  const { name, email, phone, type} = req.body;
+
+  //build contact object
+  const contactFields = {};
+  if(name) contactFields.name = name;
+  if(email) contactFields.email = email;
+  if(phone) contactFields.phone= phone;
+  if(type) contactFields.type= type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if(!contact) return res.status(404).json({msg: 'Contact not found'});
+
+    //Make sure user owns contact
+    if(contact.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'Not Authorized'});
+    }
+
+    contact = await Contact.findByIdAndUpdate(req.params.id, 
+      {$set: contactFields},
+      {new: true}
+      );
+      res.json(contact);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error'); 
+  }
+  
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
 // @access  Private
-router.delete ('/', (req, res) => {
-  res.send('Delete contacts');
+router.delete ('/:id', auth, async (req, res) => {
+
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if(!contact) return res.status(404).json({msg: 'Contact not found'});
+
+    //Make sure user owns contact
+    if(contact.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'Not Authorized'});
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+      res.json({msg: 'Contact Removed'});
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error'); 
+  }
 });
+
 
 
 module.exports = router;
