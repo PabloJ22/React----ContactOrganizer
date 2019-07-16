4f315f83dce1059db14520b98d624226cafc97b0
APabloJ <altisimop@gmail.com>
2019-07-16 13:53:52 -0500

    created Register Route and component. UI looks clean.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -11,19 +13,20 @@ import './App.css';
 const App = () => {
   return (
     <AuthState>
-    <ContactState>
-      <Router>
-        <Fragment >
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path = '/' component={Home} />
-              <Route exact path = '/about' component={About} />
-              </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+      <ContactState>
+        <Router>
+          <Fragment >
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path = '/' component={Home} />
+                <Route exact path = '/about' component={About} />
+                <Route exact path = '/register' component={Register} />
+                </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
     </AuthState>
   );
 }
